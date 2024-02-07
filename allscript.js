6dc34aa138b44e95f48d37466a318b23a962b4dd
dentnotes <155078783+dentnotes@users.noreply.github.com>
2024-02-07 22:12:47 +1000

    Update allscript.js

--- allscript.js
+++ allscript.js
@@ -1,19 +1,5 @@
 
-import { Analytics } from '@vercel/analytics/react';
- 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
+
 var finalizebool = true;
 
 function myFinalize() {
